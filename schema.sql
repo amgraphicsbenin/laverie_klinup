5b42beb10fc9e1b4b6c773646ef196df108c7567
Andre Koutomi <andrekoutomi@gmail.com>
2026-07-01 17:23:33 +0100

    feat: ajout des 30 nouveaux types de vetements dans le schema SQL Supabase

--- schema.sql
+++ schema.sql
@@ -104,10 +104,132 @@ INSERT INTO public.catalog (id, article, service, prix, categorie, description) 
 ('cat10', 'Combinaison', 'lavage_simple', 3000, 'individuel', ''),
 ('cat11', 'Combinaison', 'nettoyage_a_sec', 5000, 'individuel', ''),
 ('cat12', 'Combinaison', 'repassage', 1800, 'individuel', ''),
+-- Jupe
+('cat_jupe_ls', 'Jupe', 'lavage_simple', 0, 'individuel', ''),
+('cat_jupe_nas', 'Jupe', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_jupe_rep', 'Jupe', 'repassage', 0, 'individuel', ''),
+-- Pull
+('cat_pull_ls', 'Pull', 'lavage_simple', 0, 'individuel', ''),
+('cat_pull_nas', 'Pull', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_pull_rep', 'Pull', 'repassage', 0, 'individuel', ''),
+-- Culotte
+('cat_culotte_ls', 'Culotte', 'lavage_simple', 0, 'individuel', ''),
+('cat_culotte_nas', 'Culotte', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_culotte_rep', 'Culotte', 'repassage', 0, 'individuel', ''),
+-- T-shirt
+('cat_tshirt_ls', 'T-shirt', 'lavage_simple', 0, 'individuel', ''),
+('cat_tshirt_nas', 'T-shirt', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_tshirt_rep', 'T-shirt', 'repassage', 0, 'individuel', ''),
+-- Polo
+('cat_polo_ls', 'Polo', 'lavage_simple', 0, 'individuel', ''),
+('cat_polo_nas', 'Polo', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_polo_rep', 'Polo', 'repassage', 0, 'individuel', ''),
+-- Blouson
+('cat_blouson_ls', 'Blouson', 'lavage_simple', 0, 'individuel', ''),
+('cat_blouson_nas', 'Blouson', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_blouson_rep', 'Blouson', 'repassage', 0, 'individuel', ''),
+-- Veste
+('cat_veste_ls', 'Veste', 'lavage_simple', 0, 'individuel', ''),
+('cat_veste_nas', 'Veste', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_veste_rep', 'Veste', 'repassage', 0, 'individuel', ''),
+-- Costume
+('cat_costume_ls', 'Costume', 'lavage_simple', 0, 'individuel', ''),
+('cat_costume_nas', 'Costume', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_costume_rep', 'Costume', 'repassage', 0, 'individuel', ''),
+-- Cravate
+('cat_cravate_ls', 'Cravate', 'lavage_simple', 0, 'individuel', ''),
+('cat_cravate_nas', 'Cravate', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_cravate_rep', 'Cravate', 'repassage', 0, 'individuel', ''),
+-- Haut
+('cat_haut_ls', 'Haut', 'lavage_simple', 0, 'individuel', ''),
+('cat_haut_nas', 'Haut', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_haut_rep', 'Haut', 'repassage', 0, 'individuel', ''),
+-- Débardeur
+('cat_debardeur_ls', 'Débardeur', 'lavage_simple', 0, 'individuel', ''),
+('cat_debardeur_nas', 'Débardeur', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_debardeur_rep', 'Débardeur', 'repassage', 0, 'individuel', ''),
+-- Jeans
+('cat_jeans_ls', 'Jeans', 'lavage_simple', 0, 'individuel', ''),
+('cat_jeans_nas', 'Jeans', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_jeans_rep', 'Jeans', 'repassage', 0, 'individuel', ''),
+-- Robe de mariée
+('cat_robemariee_ls', 'Robe de mariée', 'lavage_simple', 0, 'individuel', ''),
+('cat_robemariee_nas', 'Robe de mariée', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_robemariee_rep', 'Robe de mariée', 'repassage', 0, 'individuel', ''),
+-- Couette Legée
+('cat_couettelegee_ls', 'Couette Legée', 'lavage_simple', 0, 'individuel', ''),
+('cat_couettelegee_nas', 'Couette Legée', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_couettelegee_rep', 'Couette Legée', 'repassage', 0, 'individuel', ''),
+-- Couette lourd
+('cat_couettelourd_ls', 'Couette lourd', 'lavage_simple', 0, 'individuel', ''),
+('cat_couettelourd_nas', 'Couette lourd', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_couettelourd_rep', 'Couette lourd', 'repassage', 0, 'individuel', ''),
+-- 1Draps+ 2 taies
+('cat_1draps2taies_ls', '1Draps+ 2 taies', 'lavage_simple', 0, 'individuel', ''),
+('cat_1draps2taies_nas', '1Draps+ 2 taies', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_1draps2taies_rep', '1Draps+ 2 taies', 'repassage', 0, 'individuel', ''),
+-- 2 draps+ 2 taies
+('cat_2draps2taies_ls', '2 draps+ 2 taies', 'lavage_simple', 0, 'individuel', ''),
+('cat_2draps2taies_nas', '2 draps+ 2 taies', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_2draps2taies_rep', '2 draps+ 2 taies', 'repassage', 0, 'individuel', ''),
+-- Taies
+('cat_taies_ls', 'Taies', 'lavage_simple', 0, 'individuel', ''),
+('cat_taies_nas', 'Taies', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_taies_rep', 'Taies', 'repassage', 0, 'individuel', ''),
+-- Petite serviette
+('cat_petiteserviette_ls', 'Petite serviette', 'lavage_simple', 0, 'individuel', ''),
+('cat_petiteserviette_nas', 'Petite serviette', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_petiteserviette_rep', 'Petite serviette', 'repassage', 0, 'individuel', ''),
+-- Grandes serviettes
+('cat_grandesserviettes_ls', 'Grandes serviettes', 'lavage_simple', 0, 'individuel', ''),
+('cat_grandesserviettes_nas', 'Grandes serviettes', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_grandesserviettes_rep', 'Grandes serviettes', 'repassage', 0, 'individuel', ''),
+-- Ensemble 2 pièce
+('cat_ensemble2piece_ls', 'Ensemble 2 pièce', 'lavage_simple', 0, 'individuel', ''),
+('cat_ensemble2piece_nas', 'Ensemble 2 pièce', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_ensemble2piece_rep', 'Ensemble 2 pièce', 'repassage', 0, 'individuel', ''),
+-- Ensemble 3 pièces
+('cat_ensemble3pieces_ls', 'Ensemble 3 pièces', 'lavage_simple', 0, 'individuel', ''),
+('cat_ensemble3pieces_nas', 'Ensemble 3 pièces', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_ensemble3pieces_rep', 'Ensemble 3 pièces', 'repassage', 0, 'individuel', ''),
+-- Chapeau
+('cat_chapeau_ls', 'Chapeau', 'lavage_simple', 0, 'individuel', ''),
+('cat_chapeau_nas', 'Chapeau', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_chapeau_rep', 'Chapeau', 'repassage', 0, 'individuel', ''),
+-- chausette
+('cat_chausette_ls', 'chausette', 'lavage_simple', 0, 'individuel', ''),
+('cat_chausette_nas', 'chausette', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_chausette_rep', 'chausette', 'repassage', 0, 'individuel', ''),
+-- Nappe de table
+('cat_nappetable_ls', 'Nappe de table', 'lavage_simple', 0, 'individuel', ''),
+('cat_nappetable_nas', 'Nappe de table', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_nappetable_rep', 'Nappe de table', 'repassage', 0, 'individuel', ''),
+-- Rideau
+('cat_rideau_ls', 'Rideau', 'lavage_simple', 0, 'individuel', ''),
+('cat_rideau_nas', 'Rideau', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_rideau_rep', 'Rideau', 'repassage', 0, 'individuel', ''),
+-- Robe fantaisiste
+('cat_robefantaisiste_ls', 'Robe fantaisiste', 'lavage_simple', 0, 'individuel', ''),
+('cat_robefantaisiste_nas', 'Robe fantaisiste', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_robefantaisiste_rep', 'Robe fantaisiste', 'repassage', 0, 'individuel', ''),
+-- Serpillière
+('cat_serpilliere_ls', 'Serpillière', 'lavage_simple', 0, 'individuel', ''),
+('cat_serpilliere_nas', 'Serpillière', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_serpilliere_rep', 'Serpillière', 'repassage', 0, 'individuel', ''),
+-- Torchon
+('cat_torchon_ls', 'Torchon', 'lavage_simple', 0, 'individuel', ''),
+('cat_torchon_nas', 'Torchon', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_torchon_rep', 'Torchon', 'repassage', 0, 'individuel', ''),
+-- Foulard
+('cat_foulard_ls', 'Foulard', 'lavage_simple', 0, 'individuel', ''),
+('cat_foulard_nas', 'Foulard', 'nettoyage_a_sec', 0, 'individuel', ''),
+('cat_foulard_rep', 'Foulard', 'repassage', 0, 'individuel', ''),
+-- Abonnements
 ('sub1', 'Offre Active', 'abonnement', 20000, 'abonnement', '25 vêtements | Livraison et ramassage gratuits'),
 ('sub2', 'Abonnement Premium', 'abonnement', 35000, 'abonnement', '50 vêtements max/mois | 2 ramassages max par mois | Ramassage et livraison gratuits'),
 ('sub3', 'Abonnement Prestige', 'abonnement', 60000, 'abonnement', '100 vêtements max/mois | 4 ramassages max par mois | Ramassage et livraison gratuits'),
 ('sub4', 'Abonnement VIP', 'abonnement', 100000, 'abonnement', '200 vêtements max/mois | 4 ramassages max par mois | Ramassage et livraison gratuits'),
+-- Paramètres système
 ('setting_express_hours', 'Délai Express (heures)', 'system', 6, 'system_setting', 'Configuration système'),
 ('setting_normal_hours', 'Délai Normal (heures)', 'system', 48, 'system_setting', 'Configuration système'),
 ('setting_express_markup', 'Majoration Express (%)', 'system', 50, 'system_setting', 'Configuration système')
